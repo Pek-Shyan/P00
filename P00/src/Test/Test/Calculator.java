801003755c524183fd4511bcb5409a536be4cd84
peksh <peksh@LAPTOP-IF428HND>
2022-05-27 14:11:02 +0800

    Yes

---
+++ P00/src/Test/Test/Calculator.java
@@ -0,0 +1,22 @@
+package Test.Test;
+
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+
+	public int divide(int a, int b) {
+		return a / b;
+	}
+
+}
